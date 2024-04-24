e10d0e06b0a3e4276a4cebc5e8d70c34c245e908
Peter-Awod <peterawod74@gmail.com>
2024-04-24 18:55:53 +0200

    Add submit form cubit

---
+++ lib/cubit/submit_maintenance_form_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:maintenance_app/cubit/submit_maintenance_form_states.dart';
+
+class SubmitFormCubit extends Cubit<SubmitMaintenanceFormStates> {
+  SubmitFormCubit() : super(SubmitFormInitialState());
+
+  static SubmitFormCubit get(context)=>BlocProvider.of(context);
+}
---
+++ lib/cubit/submit_maintenance_form_states.dart
@@ -0,0 +1,8 @@
+abstract class SubmitMaintenanceFormStates{}
+
+class SubmitFormInitialState extends SubmitMaintenanceFormStates{}
+
+class SubmitFormSuccess extends SubmitMaintenanceFormStates{}
+
+class SubmitFormFailure extends SubmitMaintenanceFormStates{}
+
