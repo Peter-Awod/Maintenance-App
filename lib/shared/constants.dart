f6b527f0a1114c5b64e5a476b253c5b56b03c488
Peter-Awod <peterawod74@gmail.com>
2024-04-26 18:45:55 +0200

    add constant colors

---
+++ lib/shared/constants.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor=Color(0xff2B475E);
+const Color kSecondaryColor=Color(0xffC7EDE6);
+
+
+
+// Future<void> signInWithEmailAndPassword() async {
+//   try {
+//     final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+//       email: email!,
+//       password: password!,
+//     );
+//     print('success');
+//     print(credential.user);
+//   } on FirebaseAuthException catch (e) {
+//     if (e.code == 'user-not-found') {
+//       print("No user found for that email.");
+//     } else if (e.code == 'wrong-password') {
+//       print("Wrong password provided for that user.");
+//     } else {
+//       print('Check your Email and password!');
+//     }
+//   } catch (e) {
+//     print('message erorr: ${e.toString()}');
+//   }
+// }
