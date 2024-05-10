fd413564291f510dc0995f80efb8c952532a8c9c
Peter-Awod <peterawod74@gmail.com>
2024-05-11 02:34:32 +0300

    add shimmer effect

---
+++ lib/shared/custom_widgets/shimmer_view_builder.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ShimmerViewBuilder extends StatelessWidget {
+  const ShimmerViewBuilder({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) {
+        return Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Shimmer.fromColors(
+            baseColor: Colors.blueGrey.shade200,
+            highlightColor: Colors.blueGrey.shade100,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // User widget
+                const SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  height: 28,
+                  width: 300,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(8),
+                    ),
+                    color: Colors.blueGrey,
+                  ),
+                ),
+                // User widget
+                const SizedBox(
+                  height: 16,
+                ),
+
+                Container(
+                  height: 80,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(8),
+                    ),
+                    color: Colors.blueGrey,
+                  ),
+                ),
+                // leading post widget
+                const SizedBox(height: 8),
+                Container(
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(8),
+                    ),
+                    color: Colors.blueGrey,
+                  ),
+                  height: 20,
+                  width: 240,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
